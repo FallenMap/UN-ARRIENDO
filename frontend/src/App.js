d23e49bf4c14a529b0dc2e6a55e44af597a596d4
bcolorado <bcolorado@unal.edu.co>
2022-09-04 08:40:18 -0500

    First commit with React frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hola mundo en React :D
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          No abrir!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
