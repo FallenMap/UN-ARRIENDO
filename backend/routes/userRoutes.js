691edc56290b4a98a332a63f87ce8ad52b90d62a
Mafs06 <mcabrales@unal.edu.co>
2022-10-07 18:03:51 -0500

    Commented some validation lines while the function is fixed

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,9 +8,9 @@ const { body, validationResult } = require('express-validator');
 const { validateCreate } = require('../validators/userValidator');
 
 // Create different routes for each action and call the userController functions.
-router.post('/register', upload.userPhoto, function(req, res){validateCreate},userController.createUser)
+router.post('/register', upload.userPhoto, validateCreate,userController.createUser)
     .post('/login', body('user').isEmail(), userController.loginUser)
     .post('/logout', userController.logoutUser) 
-    .put('/update', validateSession, /*validateUpdate,*/ upload.userPhoto, userController.updateUser);
+    .put('/update', validateSession, /*function(req, res){validateUpdate},*/ upload.userPhoto, userController.updateUser);
 
 module.exports = router;
