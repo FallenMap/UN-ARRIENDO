fd6f69b09193981ce32e42794c2132571d6ac33c
jhrodriguezi <70608790+Jhrodriguezi@users.noreply.github.com>
2022-10-12 20:03:39 -0500

    historial fixed

--- frontend/src/components/inside/historial.jsx
+++ frontend/src/components/inside/historial.jsx
@@ -24,6 +24,7 @@ import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import EditIcon from '@mui/icons-material/Edit';
 import { deleteListing } from "../../controllers/listingActionsController";
+import CustomizedDialogs from "./contact";
 const theme = createTheme();
 
 export function Historial() {
@@ -39,12 +40,12 @@ export function Historial() {
     getHistoryListings(auth).then(listingsResp => setListings(listingsResp));
   }, [auth]);
 
-  const handleDelete=(idlisting) =>{
-     // console.log("Funciona")
-     // console.log(idlisting)
-     deleteListing(auth,idlisting);
-     getHistoryListings(auth).then(listingsResp => setListings(listingsResp));
-    }
+  const handleDelete = (idlisting) => {
+    // console.log("Funciona")
+    // console.log(idlisting)
+    deleteListing(auth, idlisting);
+    getHistoryListings(auth).then(listingsResp => setListings(listingsResp));
+  }
 
   return (
     <>
@@ -63,102 +64,102 @@ export function Historial() {
               <Container sx={{ py: 8 }} maxWidth="md">
                 {/* End hero unit */}
                 <Grid container spacing={3}>
-                  {listings?.map((listing) => { 
+                  {listings?.map((listing) => {
                     if (listing[formAllListings.activo]) {
-                    return (<Grid item xs={6}>
-                      <Card
-                        sx={{
-                          height: "100%",
-                          display: "flex",
-                          flexDirection: "column",
-                        }}
-                      >
-                        <CardMedia
-                          component="img"
+                      return (<Grid item xs={6}>
+                        <Card
                           sx={{
-                            // 16:9
-                            pt: "2%",
-                            pl: "2%",
-                            pr: "2%",
-                            height: '300px'
+                            height: "100%",
+                            display: "flex",
+                            flexDirection: "column",
                           }}
-                          image={"http://localhost:5000/images/listing/" + listing[formAllListings.imagenes][0]}
-                          alt="random"
-                          onError={(e)=>{
-                            if(e.target.src===`http://localhost:5000/images/listing/${listing[formAllListings.imagenes][0]}`){
-                              e.target.src="https://programacion.net/files/article/20161110041116_image-not-found.png";
-                            }
-                          }}
-                        />
-                        <CardContent sx={{ flexGrow: 1 }}>
-                          <Typography
-                            gutterBottom
-                            variant="h5"
-                            component="h2"
-                          >
-                            {listing[formAllListings.titulo]}
-                          </Typography>
-                          <Typography>{listing[formAllListings.descripcion]}</Typography>
-                        </CardContent>
-                        <CardActions>
-                          <Box sx={{ padding: "0" }}>
-                            <Grid container spacing={2}>
-                              <Grid item xs>
-                                <Box display="flex"
-                                  justifyContent="center"
-                                  alignItems="center">
-                                  <Link to={`/listing/details/${listing[formAllListings.idlisting]}`} style={{ textDecoration: "none" }}>
-                                    <Button size="small">Ver más detalles</Button>
-                                  </Link>
-                                </Box>
-                              </Grid>
-                              <Grid item xs>
-                                <Box display="flex"
-                                  justifyContent="center"
-                                  alignItems="center">
-                                  <Button size="small">Contactar</Button>
-                                </Box>
-                              </Grid>
-                              <Grid item xs={12}>
-                                <Box display="flex"
-                                  justifyContent="center"
-                                  alignItems="right">
-                                  <HoverRating idListing={listing[formAllListings.idlisting]} reviewedByTenants={listing[formAllListings.valoradoEstudiantes]} value={listing[formAllListings.valoracion] || 0} />
-                                </Box>
-                              </Grid>
-                              <Grid item xs={6}>
-                                <Box display="flex"
-                                  justifyContent="center"
-                                  alignItems="center">
-                                  <Tooltip title="Delete" placement="right">
-                                    <Link to="/Historial" replace>
-                                    <IconButton onClick={() => handleDelete(listing[formAllListings.idlisting])} >
-                                      <DeleteIcon />
-                                    </IconButton>
+                        >
+                          <CardMedia
+                            component="img"
+                            sx={{
+                              // 16:9
+                              pt: "2%",
+                              pl: "2%",
+                              pr: "2%",
+                              height: '300px'
+                            }}
+                            image={"http://localhost:5000/images/listing/" + listing[formAllListings.imagenes][0]}
+                            alt="random"
+                            onError={(e) => {
+                              if (e.target.src === `http://localhost:5000/images/listing/${listing[formAllListings.imagenes][0]}`) {
+                                e.target.src = "https://programacion.net/files/article/20161110041116_image-not-found.png";
+                              }
+                            }}
+                          />
+                          <CardContent sx={{ flexGrow: 1 }}>
+                            <Typography
+                              gutterBottom
+                              variant="h5"
+                              component="h2"
+                            >
+                              {listing[formAllListings.titulo]}
+                            </Typography>
+                            <Typography>{listing[formAllListings.descripcion]}</Typography>
+                          </CardContent>
+                          <CardActions>
+                            <Box sx={{ padding: "0" }}>
+                              <Grid container spacing={2}>
+                                <Grid item xs>
+                                  <Box display="flex"
+                                    justifyContent="center"
+                                    alignItems="center">
+                                    <Link to={`/listing/details/${listing[formAllListings.idlisting]}`} style={{ textDecoration: "none" }}>
+                                      <Button size="small">Ver más detalles</Button>
                                     </Link>
-                                  </Tooltip>
-                                </Box>
+                                  </Box>
+                                </Grid>
+                                <Grid item xs>
+                                  <Box display="flex"
+                                    justifyContent="center"
+                                    alignItems="center">
+                                    <CustomizedDialogs listing={listing} />
+                                  </Box>
+                                </Grid>
+                                <Grid item xs={12}>
+                                  <Box display="flex"
+                                    justifyContent="center"
+                                    alignItems="right">
+                                    <HoverRating idListing={listing[formAllListings.idlisting]} reviewedByTenants={listing[formAllListings.valoradoEstudiantes]} value={listing[formAllListings.valoracion] || 0} />
+                                  </Box>
+                                </Grid>
+                                <Grid item xs={6}>
+                                  <Box display="flex"
+                                    justifyContent="center"
+                                    alignItems="center">
+                                    <Tooltip title="Delete" placement="right">
+                                      <Link to="/Historial" replace>
+                                        <IconButton onClick={() => handleDelete(listing[formAllListings.idlisting])} >
+                                          <DeleteIcon />
+                                        </IconButton>
+                                      </Link>
+                                    </Tooltip>
+                                  </Box>
+                                </Grid>
+                                <Grid item xs={6}>
+                                  <Box display="flex"
+                                    justifyContent="center"
+                                    alignItems="center">
+                                    <Tooltip title="Editar" placement="right">
+                                      <Link to={`/listing/update/${listing[formAllListings.idlisting]}`} style={{ textDecoration: "none" }}>
+                                        <IconButton>
+                                          <EditIcon />
+                                        </IconButton>
+                                      </Link>
+                                    </Tooltip>
+                                  </Box>
+                                </Grid>
                               </Grid>
-                              <Grid item xs={6}>
-                                <Box display="flex"
-                                  justifyContent="center"
-                                  alignItems="center">
-                                  <Tooltip title="Editar" placement="right">
-                                    <Link to={`/listing/update/${listing[formAllListings.idlisting]}`} style={{textDecoration:"none"}}>
-                                    <IconButton>
-                                      <EditIcon />
-                                    </IconButton>
-                                    </Link>
-                                  </Tooltip>
-                                </Box>
-                              </Grid>
-                            </Grid>
-                          </Box>
-                        </CardActions>
-                      </Card>
-                    </Grid>)
-                    }else{
-                      return(<></>)
+                            </Box>
+                          </CardActions>
+                        </Card>
+                      </Grid>)
+                    } else {
+                      return (<></>)
                     }
                   })}
                 </Grid>
