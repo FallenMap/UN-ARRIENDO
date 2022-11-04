43f80d694a57dc4c3ed106f1a66521cd63cece66
jhrodriguezi <70608790+Jhrodriguezi@users.noreply.github.com>
2022-11-03 20:49:34 -0500

    url changed

--- frontend/src/api/listingAPI.js
+++ frontend/src/api/listingAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 axios.defaults.withCredentials = true;
 
-const URL = "http://localhost:5000";
+const URL = "https://backend-unarriendo.herokuapp.com/";
 
 /*
     Description
