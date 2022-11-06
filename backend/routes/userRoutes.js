e0b3697a7365830d02ed09b8a93ba43b718ace99
Miguel Puentes <mipuentesc@unal.edu.co>
2022-11-06 10:12:32 -0500

    textual filter added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -15,6 +15,7 @@ router.post('/register', upload.userPhoto, validateRegisterUser,userController.c
     .post('/reviewDelete', validateSession, userController.deleteUserReview)
     .get('/get/:userPubID', userController.getUser) 
     .put('/update', validateSession, /*validationUpdateUser,*/ upload.userPhoto, userController.updateUser)
-    .get('/profile/:id', validateSession, userController.getUserProfile);
+    .get('/profile/:id', validateSession, userController.getUserProfile)
+    .get('/find', validateSession, userController.find);
 
 module.exports = router;
