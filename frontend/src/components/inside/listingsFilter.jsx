559a86dfb2a8003eb9a11d93d451df756ad57e89
bcolorado <bcolorado@unal.edu.co>
2022-11-19 11:35:33 -0500

    Added some fields in listing filter form

--- frontend/src/components/inside/listingsFilter.jsx
+++ frontend/src/components/inside/listingsFilter.jsx
@@ -9,23 +9,48 @@ import InputLabel from '@mui/material/InputLabel';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import FormControl from '@mui/material/FormControl';
 import InputAdornment from '@mui/material/InputAdornment';
-import { Checkbox, Typography } from '@mui/material';
+import {  Typography } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormLabel from '@mui/material/FormLabel';
 
+import { Radio, Switch,MenuItem, Select } from '@mui/material';
+
     
 export  function ListingsFilter() {
+
+  const [state, setState] = React.useState({
+    pet: false,
+    furniture: false,
+    park: false,
+    bici: false
+  });
+
+  const handleChangeSwitch = (event) => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+
+    const [value, setValue] = React.useState('apartment',"room","studioApartment");
+   
+
+    const handleChangeRadio = (event) => {
+      setValue(event.target.value);
+    };
     
+
+
     const [values, setValues] = React.useState({
         lowAmount: "",
         upperAmount:"",
         rooms:"",
         bathrooms:"",
-        room: "",
-        apartment: "",
-        studioApartment:"",
+        stratum:""
+
         });
 
 
@@ -88,6 +113,7 @@ export  function ListingsFilter() {
                         onChange={handleChangeAmount('rooms')}
                         label="Habitaciones"
                         type="number"
+                        inputProps={{ min: 1, max: 10 }}
                         InputLabelProps={{
                             shrink: true,
                         }}
@@ -103,6 +129,7 @@ export  function ListingsFilter() {
                         onChange={handleChangeAmount('bathrooms')}
                         label="Baños"
                         type="number"
+                        inputProps={{ min: 1, max: 5 }}
                         InputLabelProps={{
                             shrink: true,
                         }}
@@ -115,12 +142,76 @@ export  function ListingsFilter() {
                     row
                     aria-labelledby="demo-row-radio-buttons-group-label"
                     name="row-radio-buttons-group"
+                    value={value}
+                    onChange={handleChangeRadio}
+                    defaultValue="null"
                 >
-                    <FormControlLabel value={values.room} onChange={handleChangeAmount('room')} control={<Checkbox />} label="Habitación" sx={{ color: 'black' }}   />
-                    <FormControlLabel value={values.studioApartment} onChange={handleChangeAmount('studioApartment')} control={<Checkbox />} label="Apartaestudio" sx={{ color: 'black' }} />
-                    <FormControlLabel value={values.apartment} onChange={handleChangeAmount('apartment')} control={<Checkbox />} label="Apartamento" sx={{ color: 'black' }} />
+                    <FormControlLabel value="room"  control={<Radio />} label="Habitación" sx={{ color: 'black' }}   />
+                    <FormControlLabel value="studioApartment"  control={<Radio />} label="Apartaestudio" sx={{ color: 'black' }} />
+                    <FormControlLabel value="apartment"  control={<Radio />} label="Apartamento" sx={{ color: 'black' }} />
                 </RadioGroup>
             </FormControl>
+
+            <FormControl>
+            <FormLabel id="petTitle" sx={{ fontSize:"90%", mb:1 }}>Pet Friendly</FormLabel>
+              <Switch   checked={state.pet}
+              onChange={handleChangeSwitch}
+              name="pet"
+              inputProps={{ 'aria-label': 'controlled' }}/>
+            </FormControl>
+            <Typography sx={{  margin:"auto" }} >
+          
+          </Typography>
+
+            <FormControl>
+            <FormLabel id="petTitle" sx={{ fontSize:"90%", mb:1 }}>Amoblado</FormLabel>
+              <Switch   checked={state.furniture}
+              onChange={handleChangeSwitch}
+              name="furniture"
+              inputProps={{ 'aria-label': 'controlled' }}/>
+            </FormControl>
+            <Typography sx={{  margin:"auto" }} >
+          
+          </Typography>
+
+            <FormControl>
+            <FormLabel id="petTitle" sx={{ fontSize:"90%", mb:1 }}>Parqueadero</FormLabel>
+              <Switch   checked={state.park}
+              onChange={handleChangeSwitch}
+              name="park"
+              inputProps={{ 'aria-label': 'controlled' }}/>
+            </FormControl>
+
+            <Typography sx={{  margin:"auto" }} >
+          
+          </Typography>
+
+            <FormControl>
+            <FormLabel id="petTitle" sx={{ fontSize:"90%", mb:1 }}>Bicicletero</FormLabel>
+              <Switch   checked={state.bici}
+              onChange={handleChangeSwitch}
+              name="bici"
+              inputProps={{ 'aria-label': 'controlled' }}/>
+            </FormControl>
+
+            <FormControl fullWidth sx={{m:2}}>
+                    <InputLabel id="stratum-simple-select-label">Estrato</InputLabel>
+                    <Select
+                        labelId="stratum-simple-select-label"
+                        id="stratum"
+                        value={values.stratum}
+                        label="Estrato"
+                        name="stratum"
+                        onChange={handleChangeAmount('stratum')}
+                    >
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                        <MenuItem value={4}>4</MenuItem>
+                        <MenuItem value={5}>5</MenuItem>
+                        <MenuItem value={6}>6</MenuItem>
+                    </Select>
+                </FormControl>
 
           </Box>
         </DialogContent>
